4906f5d9dace5f40748b2b338d4c3cf799219a17
king-prawns <giorgio.gamberoni@gmail.com>
2020-11-20 11:47:09 +0000

    add merge two sorted array

---
+++ MergeTwoSortedArray/index.ts
@@ -0,0 +1,34 @@
+const mergeTwoSortedArray = (A1: number[], A2: number[]): number[] => {
+    const R: number[] = []
+    const L1 = A1.length;
+    const L2 = A2.length;
+    let x = 0;
+    let y = 0;
+    let k = 0;
+
+    while (x < L1 && y < L2) {
+        if (A1[x] < A2[y]) {
+            R[k] = A1[x];
+            k++;
+            x++;
+        } else {
+            R[k] = A2[y];
+            k++;
+            y++;
+        }
+    }
+
+    while (x < L1) {
+        R[k] = A1[x];
+        k++;
+        x++;
+    }
+
+    while (y < L2) {
+        R[k] = A2[y];
+        k++;
+        y++;
+    }
+
+    return R;
+}
