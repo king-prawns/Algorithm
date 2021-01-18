ed7c8d240b925eb615675c5a474b81225249f780
king-prawns <giorgio.gamberoni@gmail.com>
2021-01-18 18:52:50 +0000

    update

--- MergeSort/index.ts
+++ MergeSort/index.ts
@@ -2,13 +2,13 @@ const MergeSort = (A: number[]) => {
     const N = A.length;
     if (N < 2) return;
     const mid = N/2;
-    const R: number[] = [];
     const L: number[] = [];
+    const R: number[] = [];
     for (let i = 0; i < mid; i++) {
-        R[i] = A[i]
+        L[i] = A[i]
     }
     for (let i = mid; i < N; i++) {
-        L[i-mid] = A[i]
+        R[i-mid] = A[i]
     }
     MergeSort(L);
     MergeSort(R);
