70e0ca581e5fe60b7fd48e6bc65213f37e9478e4
king-prawns <giorgio.gamberoni@gmail.com>
2021-01-23 22:25:46 +0000

    update BubbleSort

--- BubbleSort/index.ts
+++ BubbleSort/index.ts
@@ -1,16 +1,14 @@
-const swap = (x: number, y: number,  A: number[]) => {
-    let temp = A[x];
-    A[x] = A[y];
-    A[y] = temp;
-}
+import SwapElementsInArray from '../SwapElementsInArray'
 
 const BubbleSort = (A: number[]) => {
     const N = A.length;
     for (let x = 0; x < N; x++) {
         for (let y = 0; y < (N - 1); y++) {
             if (A[y] > A[y+1]) {
-                swap(y, y+1, A);
+                SwapElementsInArray(A, y, y+1);
             }
         }
     }
 }
+
+export default BubbleSort;
