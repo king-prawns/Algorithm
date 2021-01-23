551d72d1bc2a1f098f3f9be8b2d4f1cb191e95c3
king-prawns <giorgio.gamberoni@gmail.com>
2021-01-23 22:25:13 +0000

    update InsertionSort

--- InsertionSort/index.ts
+++ InsertionSort/index.ts
@@ -1,16 +1,14 @@
-const swap = (x: number, y: number,  A: number[]) => {
-    let temp = A[x];
-    A[x] = A[y];
-    A[y] = temp;
-}
+import SwapElementsInArray from '../SwapElementsInArray'
 
 const InsertionSort = (A: number[]) => {
     const N = A.length;
     for (let x = 1; x < N; x++) {
         let y = x;
         while (y > 0 && A[y-1] > A[y]) {
-            swap(y-1, y, A);
+            SwapElementsInArray(A, y-1, y);
             y--;
         }
     }
 }
+
+export default InsertionSort;
